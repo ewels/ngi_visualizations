05367cf21074f364cf1211f6919823f43d2903ab
Phil Ewels <phil.ewels@scilifelab.se>
2014-11-19 15:28:20 +0100

    Added new functionality for Picard CollectGcBiasMetrics normalised coverage dual axis on the GC content plot

--- qualimap/gc_distribution.py
+++ qualimap/gc_distribution.py
@@ -18,7 +18,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 
-def plot_genome_fraction_coverage (gc_distribution_input, output_fn='gc_distribution', min_x='None', max_x='None', reference_label='Reference Genome (hg19)'):
+def plot_genome_fraction_coverage (gc_distribution_input, output_fn='gc_distribution', min_x='None', max_x='None', picard=None, reference_label='Reference Genome (hg19)'):
     """
     Main function. Takes input file and makes a plot.
     """
@@ -60,6 +60,30 @@ def plot_genome_fraction_coverage (gc_distribution_input, output_fn='gc_distribu
     if len(y1) == 1:
         raise EOFError ("Unable to find any data in input file")
     
+    # Load picard data if we have it
+    x3 = [0]
+    y3 = [0]
+    if picard is not None:
+        fn = os.path.realpath(picard)
+        try:
+            with open(fn, 'r') as fh:
+                for line in fh:
+                    line = line.strip()
+                    if len(line) == 0 or line[:1] == '#':
+                        continue
+                    sections = line.split(None, 6)
+                    if sections[0][:-1].isdigit() is False:
+                        continue
+                    gc = int(round(float(sections[0])))
+                    coverage = float(sections[4])
+                    if (min_x is None or gc >= min_x) and (max_x is None or gc <= max_x):
+                        x3.append(gc)
+                        y3.append(coverage)
+        except IOError as e:
+            logging.error("Could not load picard CollectGcBiasMetrics file: {}".format(fn))
+            logging.error("Skipping normalised coverage plotting")
+            pass
+    
     # Set up the plot
     fig = plt.figure(figsize=(8,3.4), tight_layout={'rect':(0,0,1,1)})
     axes = fig.add_subplot(111)
@@ -69,9 +93,22 @@ def plot_genome_fraction_coverage (gc_distribution_input, output_fn='gc_distribu
     axes.bar(x1, y1, width=1, linewidth=0.5, color="#decbe4")
     
     # Plot the reference, if we have it
+    lns = []
     if len(y2) > 1:
-        axes.plot(x2, y2, 'r--', label=reference_label)
-        axes.legend(prop={'size':8}, frameon=False, handlelength=2.5)
+        lns += axes.plot(x2, y2, 'r--', label=reference_label)
+    
+    # Plot the normalised coverage, if we have it
+    if len(y3) > 1:
+        ax2 = axes.twinx()
+        lns += ax2.plot(x3, y3, 'b--', label="Normalised Coverage")
+        ax2.set_ylim([0,2])
+        ax2.axhline(1, color='black', linestyle=':', linewidth=1)
+    
+    # Plot a legend if we need it
+    if len(y2) > 1 or len(y3) > 1:
+        labs = [l.get_label() for l in lns]
+        leg = ax2.legend(lns, labs, prop={'size':8}, handlelength=2.5)
+        leg.get_frame().set_linewidth(0.0)
 
     # Set axis limit if we need to
     if min_x is not None and max_x is not None:
@@ -85,6 +122,10 @@ def plot_genome_fraction_coverage (gc_distribution_input, output_fn='gc_distribu
     axes.tick_params(which='both', labelsize=8, direction='out', top=False, right=False)
     axes.grid(True, zorder=0, which='both', axis='y', linestyle='-', color='#EDEDED', linewidth=1)
     axes.set_axisbelow(True)
+    
+    # Now for second y axis if we have it
+    if len(y3) > 1:
+        ax2.tick_params(which='both', labelsize=8, direction='out', top=False, left=False)
 
     # Make the x and y axis labels have a %
     axes.set_yticklabels(["%.1f%%" % d for d in axes.get_yticks()])
@@ -92,8 +133,9 @@ def plot_genome_fraction_coverage (gc_distribution_input, output_fn='gc_distribu
     
     # Labels
     matplotlib.rcParams['mathtext.default'] = 'regular'
-    plt.xlabel(r"% GC Content")
-    plt.ylabel(r'Percentage of Library')
+    axes.set_xlabel(r"% GC Content")
+    axes.set_ylabel(r'Percentage of Library')
+    ax2.set_ylabel(r'Normalised Coverage')
     
     # SAVE OUTPUT
     png_fn = "{}.png".format(output_fn)
@@ -114,6 +156,8 @@ if __name__ == "__main__":
                         help="Minimum x axis limit. Use 'None' for data limit. Default: None")
     parser.add_argument("-m", "--max_x", dest="max_x", default='100',
                             help="Maximum x axis limit. Use 'None' for data limit. Default: None")
+    parser.add_argument("-p", "--picard", dest="picard", default=None,
+                            help="Picard CollectGcBiasMetrics output file to plot normalised GC content. Default: None")                            
     parser.add_argument("-r", "--ref_label", dest="reference_label", default='Reference Genome (hg19)',
                             help="Legend for the reference data if present. Default: 'Reference Genome (hg19)'")                        
     parser.add_argument("-l", "--log", dest="log_level", default='info', choices=['debug', 'info', 'warning'],
